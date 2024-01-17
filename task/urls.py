51b32efd69e8f0fdbb832db9ef4ea5357c266af9
srikanth <srikanth@active.agency>
2024-01-17 12:33:42 +0530

    updated urls

--- task/urls.py
+++ task/urls.py
@@ -4,6 +4,6 @@ from .views import task_list, task_create, task_edit
 
 urlpatterns = [
     path('', task_list, name='task_list'),
-    path('create/', task_create, name='task_create'),
-    path('<int:pk>/edit/', task_edit, name='task_edit'),
+    path('task-create/', task_create, name='task_create'),
+    path('task-edit/<int:pk>/', task_edit, name='task_edit'),
 ]
